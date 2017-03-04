0577e7a7ce629f03f13c5cc62354d0a97e2b04c0
Daniel Wei <youthdap@163.com>
2017-03-05 00:21:31 +0800

    重构代码，用更清晰的方式展示 React 特性

--- src/App.js
+++ src/App.js
@@ -1,46 +1,71 @@
 import React from 'react';
 
-/**
- * 创建包含 state 的 Component
- */
-// class App extends React.Component{
-//     render(){
-//         return <h1>Hello Lewis</h1>
-//         // return React.createElement('h1',null,'Hello Lewis')
-//     }
-// }
 
 /**
  * 创建 stateless component
  */
-// const App = () => <h1>stateless component</h1>
+const StatelessComponent = () => (
+    <div>
+        <h1>stateless component</h1>
+        <p>简单渲染无状态的 Component</p>
+    </div>
+);
 
+/**
+ * 创建包含 state 的 Component
+ */
+class ComponentExtendReact extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>stateful component</h1>
+                <p>继承自React.Component的有状态组件</p>
+            </div>
+        );
+    }
+}
+
+/**
+ * 使用 React.createElement 方法实现 render
+ */
+class RenderComponentUseReactCreateElement extends React.Component {
+    render() {
+        let title = React.createElement('h1', null, "render by createElement");
+        let desc = React.createElement('p', null, "使用 React.createElement(tagName,props,children)创建元素");
+        let root = React.createElement('div', null, title, desc);
+        return root;
+    }
+}
 
 /**
  * 多个标签的 render
  */
-// class App extends React.Component {
-//     render() {
-//         // render 方法只能返回一个标签 ,如果出现多个标签需要将他们包裹起来返回
-//         // 返回时使用 () 包裹或将父元素放在 return 语句的那一行
-//         return (
-//             <div>
-//                 <h1>h1</h1>
-//                 <h2>h2</h2>
-//             </div>
-//         )
-//     }
-// }
-
+class MultipleTagComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>multiple Tags</h1>
+                <p>render方法只接受一个父标签，因此需要将实现功能的子标签包裹到一个父级标签返回</p>
+            </div>
+        )
+    }
+}
 
 /**
  * 为 Component 增加属性
  */
-class App extends React.Component {
+class ComponentWithProps extends React.Component {
     render() {
-        let firstName = this.props.firstName
-        let lastName = this.props.lastName
-        return <h1>hello {firstName} {lastName}</h1>
+        let lastName = this.props.lastName;
+        return (
+            <div>
+                <h1>Component Props</h1>
+                <p>使用 ComponentName.propTypes 定义属性，属性类型使用 React.PropTypes.string 标识,在类型后跟.isRequired确定是否必需</p>
+                <p>属性值可以在render方法内定义局部变量关联[this.props.属性名]，也可直接使用this.props获取</p>
+                <p>可以通过 ComponentName.defaultProps 为属性设置默认值</p>
+                <p>使用组件时通过 [K=V] 制定属性值, 在页面中显示属性值 eg. {this.props.firstName} {lastName}</p>
+            </div>
+        )
     }
 }
 
@@ -48,7 +73,7 @@ class App extends React.Component {
  * 配置属性的类型、是否必须
  * @type {{firstName: (*), lastName: *}}
  */
-App.propTypes = {
+ComponentWithProps.propTypes = {
     firstName: React.PropTypes.string.required,
     lastName: React.PropTypes.string
 };
@@ -57,8 +82,14 @@ App.propTypes = {
  * 配置属性默认值
  * @type {{lastName: string}}
  */
-App.defaultProps = {
-    lastName: "wei"
+ComponentWithProps.defaultProps = {
+    lastName: "Wei"
 };
 
-export default App
+export {
+    StatelessComponent,
+    ComponentExtendReact,
+    RenderComponentUseReactCreateElement,
+    MultipleTagComponent,
+    ComponentWithProps
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as App from './App';
 
 ReactDOM.render(
-    <App firstName="lewis"/>,
-    document.getElementById('root')
+    <App.StatelessComponent/>,
+    document.getElementById('StatelessComponent')
+);
+
+ReactDOM.render(
+    <App.ComponentExtendReact/>,
+    document.getElementById('ComponentExtendReact')
+);
+
+ReactDOM.render(
+    <App.RenderComponentUseReactCreateElement/>,
+    document.getElementById('RenderComponentUseReactCreateElement')
+);
+
+ReactDOM.render(
+    <App.MultipleTagComponent/>,
+    document.getElementById('MultipleTagComponent')
+);
+
+ReactDOM.render(
+    <App.ComponentWithProps firstName="Lewis"/>,
+    document.getElementById('ComponentWithProps')
 );
